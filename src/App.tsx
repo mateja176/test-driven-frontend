3abf3a0d87d465fa9115aaa810e0b40d742bc925
Mateja Petrovic <mateja176@gmail.com>
2019-01-28 15:38:13 +0100

    Adds form

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,82 @@
 import * as React from "react";
+import * as yup from "yup";
 import "./App.scss";
+import AppStyle from "./AppStyle";
 
-const App = () => (
-  <div>
-    <div className="rocket">App</div>
-  </div>
-);
+export interface AppState {
+  email: string;
+  loading: boolean;
+  success: boolean;
+  errors: { message: string }[];
+}
+
+class App extends React.Component<{}, AppState> {
+  state = {
+    email: "",
+    loading: false,
+    success: false,
+    errors: []
+  };
+
+  removeError = (messageToRemoveBy: string) =>
+    this.setState(({ errors }) => ({
+      errors: errors.filter(({ message }) => message !== messageToRemoveBy)
+    }));
+
+  render = () => {
+    const {
+      state: { email, loading, success, errors }
+    } = this;
+
+    const emailValid = yup
+      .string()
+      .email()
+      .isValidSync(email);
+
+    return (
+      <div style={AppStyle}>
+        <h2
+          style={{
+            visibility: success ? "visible" : "hidden",
+            opacity: success ? 1 : 0,
+            transition: "opacity 1s ease-in-out"
+          }}
+        >
+          ✅ Submitted
+        </h2>
+        {errors.map(({ message }) => (
+          <h2 onClick={() => this.removeError(message)}>❌ {message}</h2>
+        ))}
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+
+            this.setState({ loading: true });
+
+            setTimeout(
+              () => this.setState({ loading: false, success: true, email: "" }),
+              2000
+            );
+
+            setTimeout(() => this.setState({ success: false }), 5000);
+          }}
+        >
+          <input
+            type="email"
+            value={email}
+            onChange={({ target: { value } }: any) =>
+              this.setState({ email: value })
+            }
+          />
+          <input
+            type="submit"
+            disabled={!email || !emailValid || loading}
+            style={{ cursor: loading ? "wait" : "default" }}
+          />
+        </form>
+      </div>
+    );
+  };
+}
 
 export default App;
