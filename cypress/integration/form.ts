997a120a29a057ce40d8f8f0e837aaf2c98428e2
Mateja Petrovic <mateja176@gmail.com>
2019-02-02 01:56:20 +0100

    Splits tests by use-case

--- cypress/integration/form.ts
+++ cypress/integration/form.ts
@@ -1,21 +1,28 @@
-describe("My First Test", function() {
-  it("Visits the Kitchen Sink", function() {
-    cy.visit("http://localhost:1234");
+describe("test driven frontend", function() {
+  it("should visit page with form", function() {
+    cy.visit("http://localhost:1234")
+  })
+  it("should get the form", function() {
+    cy.get("form")
+  })
+  it("should change the status of the button based on value of the email input", function() {
+    const submit = cy.get('input[type="submit"]')
 
-    const form = cy.get("form");
-
-    const submit = cy.get('input[type="submit"]');
-
-    submit.should("be.disabled");
+    submit.should("be.disabled")
 
     cy.get('input[type="email"]')
       .type("fake@email.com")
-      .should("have.value", "fake@email.com");
+      .should("have.value", "fake@email.com")
 
-    submit.should("be.enabled");
+    submit.should("be.enabled")
+  })
+  it("should change the state of the button to loading when the form is submitted", () => {
+    cy.get("form").submit()
 
-    cy.get("form").submit();
-
-    // expect(submit.style.cursor).to.equal("wait");
-  });
-});
+    expect(
+      cy.get('input[type="submit"]').then(button => {
+        expect(button.css("cursor")).to.equal("wait")
+      }),
+    )
+  })
+})
